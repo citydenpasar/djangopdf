4025220a5f0152825957f51ea292059322eec606
Permadi Ajus <permadiajus@gmail.com>
2022-10-20 11:28:41 +0800

    POST Method

--- pdf/views.py
+++ pdf/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from .models import Profile
 # Create your views here.
 def index(request):
-    
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email = request.POST.get("email","")
+        phone = request.POST.get("phone","")
+        summary = request.POST.get("summary","")
+        degree = request.POST.get("degree","")
+        school = request.POST.get("school","")
+        university = request.POST.get("university","")
+        previous_work = request.POST.get("previous_work","")
+        skills = request.POST.get("skills","")
+        
+        profile = Profile(name=name, email=email, phone=phone, summary=summary, degree=degree, school= school, university=university, previous_work=previous_work, skills=skills)
+        profile.save()
     context = {}
     return render(request, 'pdf/accept.html', context)
