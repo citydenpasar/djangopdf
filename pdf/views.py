86b91d6255329921ce5515188f68aed114d6c955
Permadi Ajus <permadiajus@gmail.com>
2022-10-20 11:19:34 +0800

    Building Views File

--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+def index(request):
+    
+    context = {}
+    return render(request, 'pdf/accept.html', context)
